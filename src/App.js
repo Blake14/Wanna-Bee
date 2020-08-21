ef9d34599f5f6f3fe7682fc073802209ecee067d
Blake McPherson <blake.mcpherson@moneybeecorp.com>
2020-08-21 15:43:44 -0500

    ReactJS

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return ( <
+        div className = "App-header" >
+        <
+        div className = "App" > WeBoard < /div> <
+        div className = "Menu" >
+        <
+        p1 > Menu < /p1> <
+        p1 > Help < /p1> <
+        p1 > Contact < /p1> <
+        h1 > It is { new Date().toLocaleDateString() }. < /h1> <
+        /div> <
+        /div>
+    );
+}
+
+export default App;
