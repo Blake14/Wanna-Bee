5c7e68d8e26b9a03d1e34364d61460192d97f4ed
Blake McPherson <blake.mcpherson@moneybeecorp.com>
2020-08-21 16:14:47 -0500

    Updates

---
+++ precache-manifest.c996806bcee5aa568a7937291764c86b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f0edb6ef6ae858775cd5b526a3d4bf",
+    "url": "/Wanna-Bee/index.html"
+  },
+  {
+    "revision": "762b14a6f20e01ddf70b",
+    "url": "/Wanna-Bee/static/css/main.c8a6f2be.chunk.css"
+  },
+  {
+    "revision": "36910976809a4872eff9",
+    "url": "/Wanna-Bee/static/js/2.8a3f330b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wanna-Bee/static/js/2.8a3f330b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762b14a6f20e01ddf70b",
+    "url": "/Wanna-Bee/static/js/main.04ccfbb2.chunk.js"
+  },
+  {
+    "revision": "e22cebac460376d75015",
+    "url": "/Wanna-Bee/static/js/runtime-main.10abafd8.js"
+  }
+]);
